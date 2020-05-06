5f5f606dca2ded1680a8336b9ca7363c3de3c790
Yuri Gravata <ygravata@gmail.com>
2020-05-06 19:04:40 -0300

    init show page and config cloudinary

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,43 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show]
+
+  # Create
+	def new
+    	@product = Product.new
+  end
+
+  def create
+  	@product = Product.new(product_params)
+      if @product.save
+      product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  # Read
   def index
-  # <h1><%= params[:category].present? ? params[:category] : "All weed" %></h1>
     if params[:category].present?
       @products = Product.where(category: params[:category])
     else
       @products = Product.all
     end
   end
-	def new
-    	@product = Product.new
+
+  def show
   end
 
-  def create
-  		@product = Product.new(product_params)
-  		@product.save
-  end
+
 
 	private
 
 	def product_params
 		params.require(:product).permit(:name, :description, :price, :quantity, :category)
 	end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 end
 
 
