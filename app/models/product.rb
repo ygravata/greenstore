a463296137e3fa4553f1ac00eb74382f79d9bf68
Yuri Gravata <ygravata@gmail.com>
2020-05-07 20:43:04 -0300

    adding category options

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,9 @@ class Product < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
   validates :name, presence: true
-  validates :category, presence: true
-  validates :description, presence: true
+  validates :category, presence: true, inclusion: { in: %w(Flower Edible Seed),
+    message:  "Choose one valid category!" }
+  validates :description, presence: true, length: { maximum: 500 }
   validates :price, presence: true
   validates :quantity, presence: true
 end
