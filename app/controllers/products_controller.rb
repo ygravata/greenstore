a48f47f0aec54a0f6e1972ca5a5677b4e746e6d2
Yuri Gravata <ygravata@gmail.com>
2020-05-07 21:55:21 -0300

    doing index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,6 @@ class ProductsController < ApplicationController
   end
 
   def show
-
   end
 
   def edit
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+
+Product.destroy_all
+
+array = %W[Flower Edible Seed]
+
+puts 'Creating products..'
+
+60.times do
+  product = Product.new(
+    user_id: 1,
+    name: Faker::Cannabis.buzzword,
+    description: Faker::Cannabis.health_benefit,
+    price: Faker::Number.decimal(l_digits: 2),
+    quantity:Faker::Number.number(digits: 2)
+    category: array.sample
+  )
+  product.save!
+  puts "Product #{product.id} created"
+end
+
