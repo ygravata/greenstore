7d5c8a4ed075f0488465c4587ce3fc32aabcef47
PedroMiranda <pbragamiranda@gmail.com>
2020-05-14 21:56:54 -0300

    pushing cart functionalisties

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,8 +5,8 @@ class CartsController < ApplicationController
 
 		@total = 0
 		if @cart
-			@cart.cart_products.each do |cart_product|
-			@total += cart_product.product.price * cart_product.quantity 
+			 @cart.cart_products.each do |cart_product|
+			 		@total += cart_product.product.price * cart_product.quantity 
 			end
 		end
 	end
@@ -15,7 +15,7 @@ class CartsController < ApplicationController
 		@cart = current_user.carts.find_by_status("Active")
 		authorize @cart
     @cart.update(cart_params)
-    redirect_to carts_path
+    redirect_to root_path
 	end
 
 	private
